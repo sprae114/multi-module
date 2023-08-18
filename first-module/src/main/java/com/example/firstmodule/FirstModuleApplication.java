afa4585086a51d68106a57a06508b06b9354c1fd
Yohan <sprae114@naver.com>
2023-08-18 21:48:25 +0900

    first 묘듈 생성
    첫번째 멀티 모듈 생성, web과 lombok을 사용함

---
+++ first-module/src/main/java/com/example/firstmodule/FirstModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstModuleApplication.class, args);
+    }
+
+}
---
+++ first-module/src/test/java/com/example/firstmodule/FirstModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
